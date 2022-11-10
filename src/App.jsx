00b8c4d093f3b1ed29286ea5e6d23b7bdcfda1d5
Artemiy <andrikv@gmail.com>
2022-11-10 21:47:42 +0300

    new layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import 'normalize-css'
 
 import './App.scss'
 import Header from './components/Header/Header'
-import Modal from './components/Modal/Modal'
-import Editor from './pages/Editor/Editor'
+import Sidebar from './components/Sidebar/Sidebar'
+import Home from './pages/Home'
 
 const App = () => {
 	return (
-		<div className='app-wrapper'>
-			<Header />
-			<main>
-				<Routes>
-					<Route path='/' element={<Editor />}></Route>
-					<Route path='/modal' element={<Modal />} />
-				</Routes>
-			</main>
+		<div className='app'>
+			<nav className='sidebar'>
+				<Sidebar />
+			</nav>
+			<div className='mainActionPage'>
+				<header>
+					<Header />
+				</header>
+				<main>
+					<Routes>
+						<Route path='/' element={<Home />}></Route>
+					</Routes>
+				</main>
+			</div>
 		</div>
 	)
 }
