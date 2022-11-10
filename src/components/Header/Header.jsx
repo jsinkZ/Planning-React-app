f29c062226bdd76c4eff060ed4db06d73dadfe8e
Artemiy <andrikv@gmail.com>
2022-11-10 21:46:18 +0300

    reworked header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,11 +3,7 @@ import React from 'react'
 import './Header.module.scss'
 
 const Header = () => {
-	return (
-		<header>
-			<h2> Reactive todo app </h2>
-		</header>
-	)
+	return <h2> Header </h2>
 }
 
 export default Header
