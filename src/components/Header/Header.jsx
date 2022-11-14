63daf674bb5758008af4f4d8fe4b8a0a79af7284
Artemiy <andrikv@gmail.com>
2022-11-15 01:29:25 +0300

    added search input, username and his avatar, also notifycations for future

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Avatar } from '@mui/material'
 
-import './Header.module.scss'
+import classes from './Header.module.scss'
+import SearchInput from '../SearchInput'
+import avatarURL from './../../assets/img/jsinkAvatar.jpg'
+import Notifications from './Notifycations'
 
 const Header = () => {
-	return <h2> Header </h2>
+	const style = { display: 'flex', width: '35%', border: '1px solid #F4F4F4', borderRadius: '25px' }
+
+	// TODO make user page with edit and logout
+
+	return (
+		<div className={classes.headerActions}>
+			<SearchInput style={style} />
+			<div className={classes.accountInfo}>
+				<Notifications />
+				<p> Artemiy V. </p>
+				<Avatar alt='Artemiy Voronin' src={avatarURL} />
+			</div>
+		</div>
+	)
 }
 
 export default Header
---
+++ src/components/Header/Notifycations.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import NotificationsIcon from '@mui/icons-material/Notifications'
+
+import classes from './Header.module.scss'
+
+const Notifications = () => {
+	// TODO notifications menu with on click icon open
+	return <></>
+}
+
+export default Notifications
